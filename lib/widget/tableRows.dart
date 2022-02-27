b2f478cae5dcd07542fcfe6880c22d181fb1f136
chani529 <chani529@github.com>
2022-02-27 18:51:08 +0900

    modified date height

--- lib/widget/tableRows.dart
+++ lib/widget/tableRows.dart
@@ -39,93 +39,107 @@ class _TableRowsState extends State<TableRows> {
     _setRowUIList();
     // print(height);
     // print(width);
-    return Container(
-      height: 100.0,
-      // height: 80.0 * widget.taskList.length,
-      constraints: BoxConstraints(
-        minHeight: 30,
-      ),
-      decoration: BoxDecoration(
-          border: Border.all(width: 1, color: Colors.grey.shade400)),
-      child: Stack(
-        children: [
-          Row(
-            children: [
-              for (var i = widget.startNum; i <= widget.endNum; i++)
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Text(widget.monthData[i].toString().substring(6)),
-                  ),
-                ),
-            ],
-          ),
-          Positioned(
-              top: 0,
-              left: 0,
-              width: width,
-              height: 100,
-              child: FutureBuilder(
-                future: _setRowUIList(),
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: [
-                        Container(
-                          height: 20,
-                          decoration: BoxDecoration(
-                              // color: Colors.black,
-                              border: Border.all(width: 1, color: Colors.blue)),
+    return FutureBuilder(
+        future: _setRowUIList(),
+        builder: (contexta, AsyncSnapshot snapshota) {
+          return Container(
+            height: 85.0 +
+                (snapshota.data != null && snapshota.data.length > 3
+                    ? (snapshota.data.length - 3) * 20.0
+                    : 0.0),
+            // height: 80.0 * widget.taskList.length,
+            constraints: BoxConstraints(
+              minHeight: 30,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.grey.shade400)),
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    for (var i = widget.startNum; i <= widget.endNum; i++)
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child:
+                              Text(widget.monthData[i].toString().substring(6)),
                         ),
-                        for (var item in snapshot.data)
-                          Container(
-                            height: 20,
-                            decoration: BoxDecoration(
-                                // color: Colors.black,
-                                border:
-                                    Border.all(width: 1, color: Colors.blue)),
-                            child: Row(
-                              children: [
-                                // for (var item in widget.taskList)
-                                //   Expanded(
-                                //     flex: 7,
-                                //     child: Container(
-                                //       color: Colors.blue,
-                                //       child:
-                                //           Text("[VRIS] 모시깽이 작업${item['start_date']}"),
-                                //     ),
-                                //   ),
-                                for (var task in item)
-                                  // print(task);
-                                  Expanded(
-                                    flex: task,
-                                    child: Container(
-                                      color: Colors.blue,
-                                      child: Text("${task.toString()}"),
+                      ),
+                  ],
+                ),
+                FutureBuilder(
+                    future: _setRowUIList(),
+                    builder: (context1, AsyncSnapshot snapshot1) {
+                      return Positioned(
+                          top: 0,
+                          left: 0,
+                          width: width,
+                          height: 85.0 +
+                              (snapshot1.data != null &&
+                                      snapshot1.data.length > 3
+                                  ? (snapshot1.data.length - 3) * 20.0
+                                  : 0.0),
+                          child: FutureBuilder(
+                            future: _setRowUIList(),
+                            builder: (context, AsyncSnapshot snapshot) {
+                              if (snapshot.hasData) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                          // color: Colors.black,
+                                          border: Border.all(
+                                              width: 1, color: Colors.blue)),
                                     ),
-                                  ),
+                                    for (var item in snapshot.data)
+                                      Container(
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                            // color: Colors.black,
+                                            border: Border.all(
+                                                width: 1, color: Colors.black)),
+                                        child: Row(
+                                          children: [
+                                            for (var task in item)
+                                              // print(task);
+                                              Expanded(
+                                                flex: task,
+                                                child: Container(
+                                                  // color: Colors.blue,
+                                                  decoration: BoxDecoration(
+                                                      // color: Colors.black,
+                                                      border: Border.all(
+                                                          width: 1,
+                                                          color: Colors.black)),
+                                                  child: Text(
+                                                      "${task.toString()}"),
+                                                ),
+                                              ),
 
-                                // Expanded(
-                                //   flex: 5,
-                                //   child: Container(
-                                //     color: Colors.red,
-                                //     child: Text("[VRIS] 모시깽이 작업"),
-                                //   ),
-                                // ),
-                              ],
-                            ),
-                          ),
-                      ],
-                    );
-                  } else {
-                    return Text("xxx");
-                  }
-                },
-              )),
-        ],
-      ),
-    );
+                                            // Expanded(
+                                            //   flex: 5,
+                                            //   child: Container(
+                                            //     color: Colors.red,
+                                            //     child: Text("[VRIS] 모시깽이 작업"),
+                                            //   ),
+                                            // ),
+                                          ],
+                                        ),
+                                      ),
+                                  ],
+                                );
+                              } else {
+                                return Text("xxx");
+                              }
+                            },
+                          ));
+                    }),
+              ],
+            ),
+          );
+        });
   }
 
   Future<List<List<int>>> _setRowUIList() async {
@@ -135,16 +149,12 @@ class _TableRowsState extends State<TableRows> {
 
     for (int i = 0; i < widget.taskList.length; i++) {
       var task = widget.taskList[i];
-      if (task["start_date"] < rowDate[6] && task['end_date'] > rowDate[0]) {
+      if (task["start_date"] <= rowDate[6] && task['end_date'] >= rowDate[0]) {
         tasks.add(task);
       }
     }
-    // print("tasks-->");
-    // print(tasks);
-    // get List
-    // print(rowDate.toString());
-    // print(rowDate);
     List<List<int>> taskUIList = [];
+    print(rowDate);
     for (int j = 0; j < tasks.length; j++) {
       var task = tasks[j];
       List<int> tmp = [];
@@ -152,40 +162,57 @@ class _TableRowsState extends State<TableRows> {
       // print(task['end_date']);
 
       //로직 검토
+      // start date가 이번주가 아닌경우 (이 전에 시작된 일정)
+      print(task);
       if (!rowDate.contains(task['start_date'])) {
+        // 이번 주 종료 일정이 아닌 경우
         if (!rowDate.contains(task['end_date'])) {
+          // 7칸 전부 색칠
           tmp.add(7);
         } else {
+          // 이번주 종료 이벤트 인덱스가 0부터 시작이기 때문에 +1
           tmp.add(rowDate.indexOf(task['end_date']) + 1);
+          // 종료 일 이후 빈칸 만들기 (7일 꽉 차있으면 안만들기 위함)
           if ((rowDate.indexOf(task['end_date']) + 1) != 7) {
             tmp.add(6 - rowDate.indexOf(task['end_date']));
           }
         }
       } else {
-        tmp.add(rowDate.indexOf(task['start_date']));
-        if (!rowDate.contains(task['end_date'])) {
-          tmp.add(7 - rowDate.indexOf(task['start_date']));
-        } else {
-          if (!rowDate.contains(task['end_date'])) {
-            tmp.add(7 - rowDate.indexOf(task['start_date']));
+        // 시작 인덱스가 0이면 1로 주기
+        if (rowDate.indexOf(task['start_date']) == 0) {
+          if (rowDate.indexOf(task['end_date']) == 0) {
+            tmp.add(1);
           } else {
-            tmp.add(rowDate.indexOf(task['end_date']) -
-                rowDate.indexOf(task['start_date']) +
-                1);
-
-            if (rowDate.indexOf(task['start_date']) +
-                    rowDate.indexOf(task['end_date']) -
-                    rowDate.indexOf(task['start_date']) !=
-                7) {
-              tmp.add(4 -
-                  rowDate.indexOf(task['start_date']) +
-                  rowDate.indexOf(task['end_date']) -
-                  rowDate.indexOf(task['start_date']));
+            if (!rowDate.contains(task['end_date'])) {
+              tmp.add(7);
+            } else {
+              tmp.add(rowDate.indexOf(task['end_date']) + 1);
             }
           }
+        } else {
+          tmp.add(rowDate.indexOf(task['start_date']));
+          if (!rowDate.contains(task['end_date'])) {
+            tmp.add(7 - rowDate.indexOf(task['start_date']));
+          } else if (rowDate.indexOf(task['end_date']) -
+                  rowDate.indexOf(task['start_date']) ==
+              0) {
+            tmp.add(1);
+          } else {
+            tmp.add(rowDate.indexOf(task['end_date']) -
+                rowDate.indexOf(task['start_date']));
+          }
+        }
+        // end_date가 포함되지 않았을 경우 (종료 일이 이번 주가 아닌 경우)
+        // if (!rowDate.contains(task['end_date'])) {
+        int endRow = 0;
+        for (int i = 0; i < tmp.length; i++) {
+          int element = tmp[i];
+          endRow += element;
+        }
+        if (endRow != 7) {
+          tmp.add(7 - endRow);
         }
       }
-      // print(tmp);
       taskUIList.add(tmp);
     }
     print(taskUIList);
